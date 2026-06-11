1dbfa05bad7558bde57f1257e04717225e21ab96
Tomas Hernandez <tomasgabrielh@gmail.com>
2026-06-11 17:36:41 -0300

    Se optimizo la logica en todas sus funciones agregando persistencia con base de datos en sqlite

--- logica_gestor.py
+++ logica_gestor.py
@@ -1,16 +1,47 @@
+import sqlite3
 import random
 from colorama import Back, Fore, Style, init
 init()
 
-"""
-diccionario_datos (dict): El diccionario que aloja los pares clave-valor con las cuentas y sus claves de forma segura.
-cuenta_a_editar (str): El nombre de la cuenta (clave) que el usuario desea localizar para modificar sus datos.
+conexion = sqlite3.connect("gestor.db")
+cursor = conexion.cursor()
 
-"""
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS gestor(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        cuenta TEXT NOT NULL,
+        usuario TEXT NOT NULL,
+        contrasena TEXT NULL
+    )
+''')
+conexion.commit()
 
 # CREAR NUEVA CONTRASEÑA
 
 def nueva_contrasena(diccionario_datos, user, account, password):
+
+    """
+    Registra credenciales en la base de datos y sincroniza el diccionario local. [cite: 134, 167]
+    
+    Parámetros:
+        diccionario_datos (dict): Diccionario de trabajo en memoria. [cite: 272]
+        user (str): Nombre de usuario o mail.
+        account (str): Nombre del sitio o aplicación.
+        password (str): Contraseña a guardar.
+    """
+
+    if account == "" or user == "" or password == "":
+        print("No ingresaste ningun dato..." + Fore.YELLOW + Style.RESET_ALL)
+        return False
+    
+    try:
+        cursor.execute('INSERT INTO gestor(usuario, cuenta, contrasena)VALUES(?,?,?)', (user, account, password))
+        id_generado = cursor.lastrowid
+        conexion.commit()
+    except sqlite3.Error:
+        print(f"Error de insercion a la base de datos.")
+        return
+        
     
     """
     PROPÓSITO
@@ -28,15 +59,13 @@ def nueva_contrasena(diccionario_datos, user, account, password):
     Se incluye un (if) inicial para asegurar que no se guarden campos vacios lo que hace al programa mas robusto.
     
     """
-        
-    if account == "" or user == "" or password == "":
-        print("No ingresaste ningun dato..." + Fore.YELLOW + Style.RESET_ALL)
-        return False
-    
+
     diccionario_datos[account] = {
+        "id" : id_generado,
         "usuario" : user,
         "contrasena" : password
     }
+    
     return True
     
 # EDITAR CONTRASEÑA
@@ -44,124 +73,151 @@ def nueva_contrasena(diccionario_datos, user, account, password):
 def editar_contrasena(diccionario_datos, cuenta_a_editar):
     
     """
-    PROPÓSITO
+    Actualiza el usuario y clave de un registro existente en la DB y el diccionario. [cite: 22, 230]
     
-    Actualiza el usuario o la clave de una cuenta ya existente.
-    
-    
-    ¿COMO ES SU FUNCIONAMIENTO?
-    
-    .get() -> Usamos este metodo para intentar recuperar los datos. si la cuenta no existe no devolverá None en lugar de un error.
-    Lógica condicional -> if datos_actuales: actúa como un interruptor. Si hay datos pide la nueva información si no avisa con un error. 
-    
-    
-    REASIGNACIÓN:
-    
-    Al final se sobreescribe el "casillero" de la cuenta cion el nuevo dato.
-    
+    Parámetros:
+        diccionario_datos (dict): Diccionario de sincronización.
+        cuenta_a_editar (str): Nombre de la cuenta que se desea modificar.
     """
 
     datos_actuales = diccionario_datos.get(cuenta_a_editar)
-    
+
     if datos_actuales:
-        
+
         print("Registro encontrado. Ingrese los nuevos datos." + Back.GREEN + Fore.WHITE + Style.RESET_ALL)
+
         nuevo_user = input("Ingrese los nuevos datos de usuario: ").strip().title()
         nuevo_pass = input("Ingrese la nueva contraseña: ").strip()
         
+        cursor.execute('UPDATE gestor SET usuario = ?, contrasena = ? WHERE cuenta = ?', (nuevo_user, nuevo_pass, cuenta_a_editar))
+
         diccionario_datos[cuenta_a_editar] = {"usuario" : nuevo_user, "contrasena" : nuevo_pass}
+
+        conexion.commit()
+
     else:
         print("La cuenta ingresada no existe" + Back.RED + Fore.WHITE + Style.RESET_ALL)
-        
+
 # ELIMINAR CONTRASEÑA
     
 def eliminar_contrasena(diccionario_datos, cuenta_a_eliminar):
     
     """
-    PROPÓSITO
+    Borra una credencial del sistema tras doble confirmación del usuario. [cite: 140, 233]
     
-    Eliminar una credencial especifica del sistema.
-    
-    
-    ¿COMO ES SU FUNCIONAMIENTO?
-    
-    Empleamos el operador (in) para verificar si la cuenta existe.
-    .pop() -> Es el encargado de eliminarla de la memoria de forma instantanea esto eliminando la necesidad de recorrer toda la base de datos una por una.
-    
+    Parámetros:
+        diccionario_datos (dict): Diccionario de sincronización.
+        cuenta_a_eliminar (str): Nombre de la cuenta a dar de baja.
     """
-        
-    if cuenta_a_eliminar in diccionario_datos:
-        diccionario_datos.pop(cuenta_a_eliminar)
-        print("¡Credencial eliminada con éxito!" + Back.GREEN + Fore.WHITE + Style.RESET_ALL)
+
+    confirmar_eliminacion = input("Confirma que desea eliminar esta cuenta? Esta accion no se puede revertir Si/No ").strip().lower()
+    
+    if confirmar_eliminacion == "si":
+        segunda_confirmacion = input("Realmente esta seguro? Si/No").strip().lower()
+        if segunda_confirmacion == "si":
+            cursor.execute('DELETE FROM gestor WHERE cuenta = ?', (cuenta_a_eliminar,))
+            conexion.commit()
+            if cuenta_a_eliminar in diccionario_datos:
+                diccionario_datos.pop(cuenta_a_eliminar)
+                print("¡Credencial eliminada con éxito!" + Back.GREEN + Fore.WHITE + Style.RESET_ALL)
+            else:
+                print(f"Error: La cuenta '{cuenta_a_eliminar}' no se encontró en el sistema." + Back.RED + Fore.WHITE + Style.RESET_ALL)
+            print(f"Registro eliminado...")
     else:
-        print(f"Error: La cuenta '{cuenta_a_eliminar}' no se encontró en el sistema." + Back.RED + Fore.WHITE + Style.RESET_ALL)
+        print(f"No se elimino el registro.")
+
+# CARGAR DATOS
+
+def cargar_datos():
+    """
+    Consulta la tabla 'gestor' y reconstruye el diccionario en memoria. [cite: 14, 15]
+    
+    Retorna:
+        dict: Diccionario sincronizado con los datos persistentes del disco. [cite: 133]
+    """
+
+    cursor.execute('SELECT * FROM gestor')
+    filas = cursor.fetchall()
+
+    diccionario_cargado = {}
+    for f in filas:
+        diccionario_cargado[f[1]] = {
+            "id" : f,
+            "usuario" : f[2],
+            "contrasena" : f[3]
+        }
+        return diccionario_cargado
     
 # VER CONTRASEÑA
     
 def ver_contrasena(diccionario_datos):
     
     """
-    PROPÓSITO
+    Muestra en pantalla todas las credenciales cargadas de forma ordenada. [cite: 133, 134]
     
-    Muestra las cedenciales guardadas de forma ordenada.
-    
-    
-    ¿COMO ES SU FUNCIONAMIENTO?
-    
-    .items() -> Es el encargado de desempaquetar el diccionario en dos variables "cuenta (la llave)" y "datos (el valor / mini-diccionario)"
-    
-    ACCESO:
-    
-    Internamente en el bucle, entramnos a las llaves "usuario" y "contraseña" para imprimir la informacion final.
-    
+    Parámetros:
+    diccionario_datos (dict): El diccionario que contiene la información.
     """
     
+    if not diccionario_datos:
+        print(Fore.RED + "No hay credenciales guardadas." + Style.RESET_ALL)
+        return
+    
     for cuenta, datos in diccionario_datos.items():
-        print(f"Cuenta: {cuenta} | Usuario: {datos['usuario']} | Contraseña: {datos['contrasena']}" + Fore.YELLOW + Style.RESET_ALL)
-        
-        
+        print(f"Cuenta: {cuenta} | Usuario: {datos['usuario']} | Contraseña: {datos['contrasena']}" + {Fore.YELLOW} + {Style.RESET_ALL})
+
 # BUSCAR CONTRASEÑA
 
 def buscar_contrasena(diccionario_datos, cuenta_buscada):
     
-    encontrado = False
+    """
+    Realiza una consulta filtrada en la base de datos para hallar una cuenta específica. [cite: 15, 218]
+    
+    Parámetros:
+        cuenta_buscada (str): Nombre de la cuenta a localizar.
+    """
 
-    resultado = diccionario_datos.get(cuenta_buscada)
+    cursor.execute('SELECT * FROM gestor WHERE cuenta = ?', (cuenta_buscada,))
+    cuenta_encontrada = cursor.fetchone()
+    
+    if cuenta_encontrada:
+        print(f"Cuenta encontrada - Usuario: {cuenta_encontrada[2]} | Contraseña: {cuenta_encontrada[3]}")
+    else: 
+        print("Registro no encontrado.")
 
-    if resultado:
-        print(f"Usuario: {resultado['usuario']}" + Fore.YELLOW + Style.RESET_ALL)
-    else:
-        print("No se encontró el registro." + Back.RED + Fore.WHITE + Style.RESET_ALL)
-        
-    if encontrado == False:
-        print("No se encontraron registros asociados a tu busqueda" + Back.RED + Fore.WHITE + Style.RESET_ALL)
-        
 # GENERAR SUGERENCIA
 
 def generar_sugerencia():
+    
+    """
+    Genera una cadena aleatoria de caracteres para proponer como contraseña segura. [cite: 169, 265]
+    
+    Retorna:
+        str: Una contraseña sugerida de alta complejidad.
+    """
+
     mayusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     minusculas = "abcdefghijklmnopqrstuvwxyz"
     numeros = "1234567890"
     simbolos_y_signos = "!@#$%^&*()_+=-{ }[ ]';/.?<>,\|"
     
+    todos = mayusculas + minusculas + numeros + simbolos_y_signos
+
     while True:
         solicitud = input("¿Cuántos caracteres desea para su contraseña? ")
         if solicitud.isdigit():
             longitud_numero = int(solicitud)
             break
-        else:
-            print("ERROR: Debes ingresar solo números. Intenta de nuevo.")
-    
-    
-    if longitud_numero < 5:
+        print("ERROR: Debes ingresar solo números. Intenta de nuevo.")
+
+    if longitud_numero < 8:
         print(f"Contraseña muy insegura{Fore.RED + Style.RESET_ALL}")
-    elif longitud_numero <= 9:
+    elif longitud_numero <= 12:
         print(f"Contraseña insegura{Fore.RED + Style.RESET_ALL}")
-    elif longitud_numero <= 15:
-        print(f"Contraseña poco segura{Fore.YELLOW + Style.RESET_ALL}")
     else:
         print(f"Contraseña segura{Fore.GREEN + Style.RESET_ALL}")
-    
-    caracteres = mayusculas + minusculas + numeros + simbolos_y_signos
-    clave = "".join(random.choice(caracteres) for _ in range(longitud_numero))
+        
+    clave = "".join(random.choice(todos) for _ in range(longitud_numero))
     return clave
+
+conexion.close()
